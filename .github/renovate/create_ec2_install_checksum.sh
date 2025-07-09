d12f97bd11f948a90340fb67b3a34cbf96679035
Kevin Davin <kdavin@gradle.com>
2025-07-09 12:03:45 +0200

    chore(renovate): adjust post upgrade tasks to allow automatic sha-sum update
    A simplification of the checksum script has been made to be compatible with renovate
    update process and eventually installations without specific binaries (`msg`).
    
    Related to #51910

--- .github/renovate/create_ec2_install_checksum.sh
+++ .github/renovate/create_ec2_install_checksum.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
-# Create sha256 checksum for the installation script
 
-set -Eeuo pipefail
+echo "Update sha256 checksum file for AWS script"
 
-setup_colors() {
-  if [[ -t 2 ]] && [[ -z "${NO_COLOR-}" ]] && [[ "${TERM-}" != "dumb" ]]; then
-    NOFORMAT='\033[0m' RED='\033[0;31m' GREEN='\033[0;32m' ORANGE='\033[0;33m' BLUE='\033[0;34m' PURPLE='\033[0;35m' CYAN='\033[0;36m' YELLOW='\033[1;33m'
-  else
-    NOFORMAT='' RED='' GREEN='' ORANGE='' BLUE='' PURPLE='' CYAN='' YELLOW=''
-  fi
-}
-
-setup_colors
-
-# Script logic
-msg "${YELLOW}Creating checksum${NOFORMAT}"
+cd "AWS/EC2"
 sha256sum ec2-install.sh > ec2-install.sh.sha256
---
+++ .github/renovate/post-upgrade.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+bash .github/renovate/create_ec2_install_checksum.sh
